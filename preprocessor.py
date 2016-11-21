7a2d9f806a1ce5b768c712e5bd02534d56859af7
Mitchell Troy <mtroy2@nd.edu>
2016-11-21 00:02:33 -0500

    Updated file import locations using os package

--- preprocessor.py
+++ preprocessor.py
@@ -1,12 +1,14 @@
+
+import os
 def main():
-	sarc_file = open('sarcasm.txt')
-	ser_file = open('serious.txt')
-	sarc_out = open('clean_sarcasm.txt', 'w+')
-	ser_out = open('clean_serious.txt', 'w+')
+	sarc_file = open(os.getcwd() + '/Data/sarcasm.txt')
+	ser_file = open(os.getcwd() + '/Data/serious.txt')
+	sarc_out = open(os.getcwd() + '/Data/clean_sarcasm.txt', 'w+')
+	ser_out = open(os.getcwd() + '/Data/clean_serious.txt', 'w+')
 	clean_files(sarc_file, sarc_out)
 	clean_files(ser_file, ser_out)
 
-def clean_files(in_file, out_file)
+def clean_files(in_file, out_file):
 	rev = False
 	s = ''
 	for line in in_file:
